a2072a87aa0c86ce99be84a010a30e6292697eb6
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-04-20 14:57:06 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
 import React,{ Fragment, useState, useEffect } from 'react';
-import './App.css';
+import './styles/main.css';
 
 
-//-38.718433213708806, -62.26625760978698
-const LAT = -38.718433213708806
-const LON = -62.26625760978698
 
-const API_KEY = 'c3c1d52d35c864fc990df40d28c02f87'
-const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}`
+
+const API_KEY = 'e01295aee6c84749a40110748222004'
+//const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchLocation}`
+//Api forecast url
+//const FC_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=London&days=3&aqi=no&alerts=no`
 
 function App() {
 
   const [datos, setDatos] = useState({})
-  const [location, setLocation] = useState('')
+  const [searchLocation, setSearchLocation] = useState('')
 
-  useEffect(() => {
-    fetch(API_URL)
+  const API_SEARCH = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchLocation}`
+
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault()
+
+    fetch(API_SEARCH)
       .then(res => res.json())
       .then((res) => {
         setDatos(res)
         console.log(res)
       })
-  },[])
+      .catch((err) => (
+        console.log(err)
+      ))
+  }
+
+  const handleOnChange = (event) => {
+    setSearchLocation(event.target.value)
+  }
+
 
 
   return (
     <Fragment>
+      <nav>
+        <div className='search-container'>
+          <form action="" onSubmit={handleOnSubmit}>
+            <input 
+              className='search' 
+              type="text" name='' id='' 
+              placeholder='City...'
+              value={searchLocation}
+              onChange={handleOnChange}
+              required
+            />
+            <button type="submit" className='search-btn'>
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </form>
+        </div>
+      </nav>
       <div>
-        <h1>Location: {datos.name}</h1>
-        {datos.main ? <h2>{datos.main.temp}</h2> : null}
+        <h1>{}</h1>
       </div>
     </Fragment>    
   );
