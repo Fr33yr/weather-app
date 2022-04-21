14a93deda96588d4abf7c22e7fa8661b7e64c809
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-04-21 17:42:02 -0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -64,6 +64,13 @@ function App() {
         datos.map((dato, index)=> <Weather key={index} {...dato}/>)
       }
       </div>
+
+
+      <footer>
+        <a href="https://www.weatherapi.com/" title="Free Weather API">
+          <img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"/>
+        </a>
+      </footer>
     </Fragment>    
   );
 }
